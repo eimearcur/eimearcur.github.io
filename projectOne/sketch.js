5145c31d657ccb50d92df8a65c1b33ac8bb1086a
eimear <eimearcurrie@gmail.com>
2018-09-17 14:07:10 -0600

    projectOne

--- projectOne/sketch.js
+++ projectOne/sketch.js
@@ -8,19 +8,36 @@
 
 let y;
 let dy;
+let x;
+let y;
 let rectwidth;
+let rectheight;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   y = height/2;
+  x = width/2
+  dx = 5
   dy = 5;
   rectwidth = 50;
+  rectheight = 50;
 
 }
 
 function draw() {
   background(9, 75, 160);
-  y += dy
+  y += dy;
+  x += dx;
+
+  if (y > width - rectheight || y < 0){
+    dy = dy * -1;
+  }
+
+  }
+
+  fill(255, 234, 53);
+  rect(x, y, rectwidth, rectheight)
+
 
 
 
