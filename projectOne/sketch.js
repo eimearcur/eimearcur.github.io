985efd6417217b9f2d6313d6d1a8c8adbd1049c8
eimear <eimearcurrie@gmail.com>
2018-09-17 13:55:46 -0600

    project one

---
+++ projectOne/sketch.js
@@ -0,0 +1,28 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+let y;
+let dy;
+let rectwidth;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  y = height/2;
+  dy = 5;
+  rectwidth = 50;
+
+}
+
+function draw() {
+  background(9, 75, 160);
+  y += dy
+
+
+
+
+}
