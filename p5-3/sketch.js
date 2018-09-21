2e934096e6668c9bf9f37f20bc620c7982254fac
eimear <eimearcurrie@gmail.com>
2018-09-21 14:01:40 -0600

    updated project

--- p5-3/sketch.js
+++ p5-3/sketch.js
@@ -1,24 +1,47 @@
-// Project Title
-// Your Name
-// Date
+// Interactive Scene Project
+// Eimear Currie
+// Sept. 24/18
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let plane;
+let planeHeight;
+
+function preload(){
+  plane = loadImage("assets/airplane.png");
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  planeHeight = height/2;
 }
 
 function draw() {
-  background(119, 214, 255);
-
-  rect(width, height, width, height)
-  fill(137, 252, 106)
-  noStroke()
-
+  background(84, 229, 255);
+  if (mouseIsPressed){
+    background(9, 69, 165);
+  }
+  image(plane, mouseX, planeHeight);
 }
 
-function grass(){
-  rect(0, 0, 50, 70);
-  fill(137, 252, 106)
+function wall(){
+  // function to make the plane not exit the screen
+  if (planeHeight > height - plane.height){
+    planeHeight -= 50;
+  }
+  else if (planeHeight < 0){
+    planeHeight += 50;
+  }
+}
+
+function keyPressed(){
+  // move the plane up and down using the arrow keys
+  if (keyCode === UP_ARROW){
+    planeHeight -= 50;
+  }
+  else if (keyCode === DOWN_ARROW){
+    planeHeight += 50;
+  }
+  wall();
 }
