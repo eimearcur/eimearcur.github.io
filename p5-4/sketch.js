ebc872f68d063ec44e1ff4049dda14a927806f4d
eimear <eimearcurrie@gmail.com>
2018-10-04 14:09:21 -0600

    states

---
+++ p5-4/sketch.js
@@ -0,0 +1,16 @@
+// Project Title
+// eimear currie
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(12, 0, 68);
+}
+
+function draw() {
+  fill(255, 0, 144);
+  rect(width/4, height/8, width/2, width/2);
+}
