83e6838b177bbced8bb91a1de9c90de45d52b4f7
eimearcur <eimearcurrie@gmail.com>
2019-01-16 18:35:47 -0600

    chose character

--- majorProject/p5/sketch.js
+++ majorProject/p5/sketch.js
@@ -18,15 +18,18 @@
 
 
 let newDanielle;
-let newSecond;
+let soos;
 let backgroundImage;
 let newDanielleHealth = 100;
-let newSecondHealth = 100;
+let soosHealth = 100;
 let state = 0;
 let hit = false;
 let playButton;
 let danielle_dy = 5;
 let second_dy = 5;
+
+
+let sontaye;
 
 function preload(){
 
@@ -34,7 +37,7 @@ function preload(){
 
   backgroundImageTwo = loadImage("assets/backgroundTwo.png");
 
-  song1 = loadSound("assets/playingBackgroundSong.mp3");
+  //song1 = loadSound("assets/playingBackgroundSong.mp3");
 
   font = loadFont("assets/ARCADECLASSIC.TTF");
 
@@ -50,11 +53,22 @@ function preload(){
   //newDanielle.addAnimation("block", "assets/danielle_1.png");
   newDanielle.setCollider("rectangle", 0, 0, 25, 25);
 
-  newSecond = createSprite(1600, 800);
-  newSecond.addAnimation("test", "assets/sprite_0.png");
-  newSecond.addAnimation("fight", "assets/sprite_1.png");
-//  newSecond.addAnimation("block", "assets/second_0.png");
-  newSecond.setCollider("rectangle", 0, 0, 25, 25);
+  alien = createSprite(400, 800);
+  alien.addAnimation("test", "assets/alien_1.png");
+  alien.addAnimation("test", "assets/alien_2.png");
+  alien.setCollider("rectangle", 0, 0, 25, 25);
+
+
+  soos = createSprite(1600, 800);
+  soos.addAnimation("test", "assets/sprite_0.png");
+  soos.addAnimation("fight", "assets/sprite_1.png");
+//  soos.addAnimation("block", "assets/second_0.png");
+  soos.setCollider("rectangle", 0, 0, 25, 25);
+
+  sontaye = createSprite(1600, 800);
+  sontaye.addAnimation("test", "assets/ghost_0.png");
+  sontaye.addAnimation("fight", "assets/ghost_1.png");
+  sontaye.setCollider("rectangle", 0, 0, 25, 25);
 
   menuDanielle = loadAnimation("assets/danielleOne.png", "assets/danielleTwo.png");
   menuSecond = loadAnimation("assets/sprite_0.png", "assets/sprite_1.png");
@@ -72,7 +86,7 @@ function draw() {
   if (state === 1) {
     background(backgroundImage);
 
-    song1.loop();
+    //song1.loop();
 
 
     movePlayerTwo();
@@ -86,14 +100,14 @@ function draw() {
     fill(255, 0, 0);
     textFont(font);
     text(newDanielleHealth, 100, 100);
-    text(newSecondHealth, 1800, 100);
+    text(soosHealth, 1800, 100);
 
     drawSprite(newDanielle);
-    drawSprite(newSecond);
+    drawSprite(soos);
     objects.add(newDanielle);
-    objects.add(newSecond);
-    newSecond.bounce(newDanielle);
-    newDanielle.bounce(newSecond);
+    objects.add(soos);
+    soos.bounce(newDanielle);
+    newDanielle.bounce(soos);
   }
   else if (state === 3){
     background(backgroundImageTwo);
@@ -109,20 +123,26 @@ function draw() {
     fill(255, 0, 0);
     textFont(font);
     text(newDanielleHealth, 100, 100);
-    text(newSecondHealth, 1800, 100);
+    text(soosHealth, 1800, 100);
 
     drawSprite(newDanielle);
-    drawSprite(newSecond);
+    drawSprite(soos);
     objects.add(newDanielle);
-    objects.add(newSecond);
-    newSecond.bounce(newDanielle);
-    newDanielle.bounce(newSecond);
+    objects.add(soos);
+    soos.bounce(newDanielle);
+    newDanielle.bounce(soos);
 
   }
   else if (state === 0) {
     background(menuBackground);
     menu();
   }
+  else if (state === 4){
+    pickPlayers()
+  }
+  // else if (state === 5){
+  //
+  // }
   else if (state === 2){
     decision();
   }
@@ -155,10 +175,48 @@ function menu(){
 
   if (hit && mouseIsPressed){
 
-    state = 2;
+    state = 4;
 
 }
 }
+
+function pickPlayers(){
+  background(menuBackground);
+  textFont(font);
+  textSize(150);
+  fill(0, 0, 255);
+  text("CHOOSE  PLAYERS", 200, 300);
+
+  if (key === "h"){
+    state =2;
+  }
+
+  else if (key === "j"){
+    textSize(100);
+    fill(0,0,255)
+    text("no", width/2, height/2);
+  }
+}
+
+// function pickPlayerTwo(){
+//   background(menuBackground);
+//   textFont(font);
+//   textSize(150);
+//   fill(0, 0, 255);
+//   text("CHOOSE  PLAYER  2", 200, 300);
+//
+//   if (key === "c"){
+//     soos = soos;
+//     state = 1;
+//   }
+//   else if (key === "v"){
+//     soos = sontaye;
+//     state =1;
+//   }
+//
+//
+//
+// }
 
 function decision(){
   background(menuBackground);
@@ -195,15 +253,15 @@ function danielleFightBlock(){
 
     if (key === "e"){
       newDanielle.changeAnimation("fight");
-      if (newDanielle.collide(newSecond)){
-        newSecondHealth -= 1;
+      if (newDanielle.collide(soos)){
+        soosHealth -= 1;
 }
     }
     else if (key === "r"){
 
       newDanielle.changeAnimation("fight");
-      if(newDanielle.collide(newSecond)){
-      newSecond.position.x += 20;
+      if(newDanielle.collide(soos)){
+      soos.position.x += 20;
     }
   }
     else{
@@ -214,30 +272,30 @@ function danielleFightBlock(){
   function secondFightBlock(){
 
       if (key === "m"){
-        newSecond.changeAnimation("fight");
-        if (newSecond.collide(newDanielle)){
+        soos.changeAnimation("fight");
+        if (soos.collide(newDanielle)){
           newDanielleHealth -= 1;
   }
       }
       else if (key === "n"){
 
-        newSecond.changeAnimation("fight");
-        if(newSecond.collide(newDanielle)){
+        soos.changeAnimation("fight");
+        if(soos.collide(newDanielle)){
         newDanielle.position.x -= 20;
       }
     }
       else{
-        newSecond.changeAnimation("test");
+        soos.changeAnimation("test");
       }
     }
 
 
 function movePlayerTwo(){
   if(keyIsDown(39)){
-    newSecond.position.x += 5;
+    soos.position.x += 5;
   }//right arrow
   if(keyIsDown(37)){ //left key
-    newSecond.position.x -= 5;
+    soos.position.x -= 5;
   }
 }
 
@@ -260,21 +318,22 @@ function danielleJump(){
 }
 
 function secondJump(){
-  newSecond.position.y += second_dy;
+  soos.position.y += second_dy;
   second_dy += 0.15;
-  if (newSecond.position.y >= 800){
+  if (soos.position.y >= 800){
     second_dy = 0;
   }
 }
 
 
 function keyPressed(){
+
   if (newDanielle.position.y >=800 ){
     if (keyCode === 32){
         danielle_dy -=9;
     }
   }
-  if (newSecond.position.y >=800 ){
+  if (soos.position.y >=800 ){
     if (keyCode === UP_ARROW){
         second_dy -=9;
 }
